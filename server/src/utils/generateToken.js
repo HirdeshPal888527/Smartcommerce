717fe73165aab8f0dc90d587df14688b709059ec
Hirdesh Pal <iitghirdesh@gmail.com>
2026-07-07 10:52:20 +0530

    Deployment Changes

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -14,7 +14,7 @@ function generateTokenAndSetCookie(user,res){
     );
     res.cookie("token",token,{
         httpOnly: true,
-        secure: process.env.NODE_ENV=== "production",
+        secure: true,
         sameSite:"lax",
         maxAge: 7*24*60*60*1000,
     });
